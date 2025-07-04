24b6473a7b89ed4d15ccf15784bc078a33183ee5
MariaLuizaLopes02 <m.lopes02@escola.pr.gov.br>
2025-07-04 17:17:44 -0300

    aaaaa

--- pergunts.js
+++ pergunts.js
@@ -1,53 +1,53 @@
 criaCartao(
     'Química',
-    'O que é uma reação exotérmica?',
+    'O que é uma reação exotérmica',
     'É aquela que libera energia para o meio.'
 )
 
 criaCartao(
     'Química',
-    'O que é uma reação exotérmica?',
+    'O que é uma reação exotérmica',
     'É aquela que absorve energia do meio.'
 )
 
 criaCartao(
     'Química',
-    'O que há em um átomo?',
+    'O que há em um átomo',
     'Elétrons, prótons e neutrons.'
 )
 
 criaCartao(
     'Química',
-    'O que a química orgânica estuda?',
+    'O que a química orgânica estuda',
     'Os compostos de carbono.'
 )
 
 criaCartao(
     'Física',
-    'Quais as três leis de Newton?',
+    'Quais as três leis de Newton',
     'A Lei da Inércia, Fr= m x a e a Lei da Ação e Reação.'
 )
 
 criaCartao(
     'História',
-    'Quando começou a revolução industrial?',
+    'Quando começou a revolução industrial',
     'Na segunda metade do século XVIII.'
 )
 
 criaCartao(
     'Quimica',
-    'Qual é o nome do elemento químico n°110 ?',
+    'Qual é o nome do elemento químico n°110',
     'O nome do elemento é Darmstádtio.'
 )
 
 criaCartao(
     'Quimica',
-    'O que a cinética estuda?',
+    'O que a cinética estuda',
     'A velocidade das reações e os fatores que a influenciam.'
 )
 
 criaCartao(
-    'Física',
-    'Qual a velocidade da luz em m/s ?',
-    'Sua velocidade é de 299.793.458 m/s.'
+    'Química',
+    'Quantas ligações o carbono precisa fazer',
+    'Ele precisa fazer 4 ligações para ficar estável.'
 )
