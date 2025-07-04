e468d6bf92d1f460e01d5cf488cf74d25ea60f5a
MariaLuizaLopes02 <m.lopes02@escola.pr.gov.br>
2025-07-04 12:07:50 -0300

    to surtando

--- pergunts.js
+++ pergunts.js
@@ -1,23 +1,53 @@
 criaCartao(
-    'Qúimica',
-    'O que é uma reação exotérmica?',
-    'É aquela que libera energia.'
+    'Português',
+    'O que é parafrasear?',
+    'expressar a mesma ideia de um texto ou frase utilizando outras palavras, sem alterar o significado original'
+)
+
+criaCartao(
+    'Português',
+    'Qual é o verbo da palavra ponderar no pretérito mais que perfeito?',
+    'nós ponderáramos.'
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'O Brasil é maior que os Estados Unidos?',
+    'Não, O Brasil com cerca de 8.515.767 km² e os Estados Unidos aproximadamente 9.833.517 km².'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Geografia',
+    'Qual á menor fronteira do mundo em terra?',
+    'entre Marrocos e Espanha, na região de Peñón de Vélez de la Gomera, com apenas 85 metros de comprimento.'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'História',
+    'Hitler nasceu aonde?',
+    'Hitler nasceu na Austria mesmo tendo trabalhado como um soldado alemão.'
+)
+
+criaCartao(
+    'História',
+    'quando começou a revolução industrial?',
+    'Na segunda metade do século XVIII, mais precisamente a partir de 1760.'
+)
+
+criaCartao(
+    'Quimica',
+    'Qual é o nome do elemento químico n°110 ?',
+    'O nome do elemento é Darmstádtio.'
+)
+
+criaCartao(
+    'Quimica',
+    'Calcule, ápos 18 horas, quantos mols ainda não terão decaído, considerando a variação temporal da constante de decaimento λ(t)? ( Use a formula N(t) = N₀ * e^(-λt))',
+    'Calculando a integral da taxa variável, obtemos um valor total de aproximadamente 1.042. Logo: N(18) = 13 * e^(-1.042)= 13 x 0,352 = 4,576 mols restantes.'
+)
+
+criaCartao(
+    'Física',
+    'Qual a velocidade da luz em m/s ?',
+    'Sua velocidade é de 299.793.458 m/s.'
 )
