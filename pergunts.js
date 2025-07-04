937b376d7f40444735647c286eec8d1a17590999
MariaLuizaLopes02 <m.lopes02@escola.pr.gov.br>
2025-07-04 12:25:19 -0300

    perguntas

--- pergunts.js
+++ pergunts.js
@@ -1,37 +1,37 @@
 criaCartao(
-    'Português',
-    'O que é parafrasear?',
-    'expressar a mesma ideia de um texto ou frase utilizando outras palavras, sem alterar o significado original'
+    'Química',
+    'O que é uma reação exotérmica?',
+    'É aquela que libera energia para o meio.'
 )
 
 criaCartao(
-    'Português',
-    'Qual é o verbo da palavra ponderar no pretérito mais que perfeito?',
-    'nós ponderáramos.'
+    'Química',
+    'O que é uma reação exotérmica?',
+    'É aquela que absorve energia do meio.'
 )
 
 criaCartao(
-    'Geografia',
-    'O Brasil é maior que os Estados Unidos?',
-    'Não, O Brasil com cerca de 8.515.767 km² e os Estados Unidos aproximadamente 9.833.517 km².'
+    'Química',
+    'O que há em um átomo?',
+    'Elétrons, prótons e neutrons.'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual á menor fronteira do mundo em terra?',
-    'entre Marrocos e Espanha, na região de Peñón de Vélez de la Gomera, com apenas 85 metros de comprimento.'
+    'Química',
+    'O que a química orgânica estuda?',
+    'Os compostos de carbono.'
+)
+
+criaCartao(
+    'Física',
+    'Quais as três leis de Newton?',
+    'A Lei da Inércia, Fr= m x a e a Lei da Ação e Reação.'
 )
 
 criaCartao(
     'História',
-    'Hitler nasceu aonde?',
-    'Hitler nasceu na Austria mesmo tendo trabalhado como um soldado alemão.'
-)
-
-criaCartao(
-    'História',
-    'quando começou a revolução industrial?',
-    'Na segunda metade do século XVIII, mais precisamente a partir de 1760.'
+    'Quando começou a revolução industrial?',
+    'Na segunda metade do século XVIII.'
 )
 
 criaCartao(
@@ -42,8 +42,8 @@ criaCartao(
 
 criaCartao(
     'Quimica',
-    'Calcule, ápos 18 horas, quantos mols ainda não terão decaído, considerando a variação temporal da constante de decaimento λ(t)? ( Use a formula N(t) = N₀ * e^(-λt))',
-    'Calculando a integral da taxa variável, obtemos um valor total de aproximadamente 1.042. Logo: N(18) = 13 * e^(-1.042)= 13 x 0,352 = 4,576 mols restantes.'
+    'O que a cinética estuda?',
+    'A velocidade das reações e os fatores que a influenciam.'
 )
 
 criaCartao(
